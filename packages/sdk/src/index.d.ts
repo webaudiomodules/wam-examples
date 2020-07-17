a3f82617bd3d9f59fe8843eb535240020ab264f3
Fr0stbyteR <renshihong@hotmail.com>
2020-07-18 00:35:15 +0800

    fix: make the sdk buildable

--- packages/sdk/src/index.d.ts
+++ packages/sdk/src/index.d.ts
@@ -1,8 +1,7 @@
 export { default as CompositeAudioNode } from './ParamMgr/CompositeAudioNode';
 export { default as ParamMgrNode } from './ParamMgr/ParamMgrNode';
 export { default as WebAudioModule } from './WebAudioModule';
-export { default as WamProcessor } from './WamProcessor';
-export { WamParameterSab, WamParameterNoSab, WamParameter } from './WamParameter';
+export { WamParameterSab, WamParameterNoSab } from './WamParameter';
 export { default as WamParameterInfo } from './WamParameterInfo';
 export { default as WamNode } from './WamNode';
 export * as Loader from './Loader';
