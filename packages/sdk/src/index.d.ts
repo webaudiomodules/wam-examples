c99e40720a1c807e50904e796fd9b2148626a5c6
Fr0stbyteR <renshihong@hotmail.com>
2020-07-30 00:57:13 +0800

    remove WamParameter in sdk esm export

--- packages/sdk/src/index.d.ts
+++ packages/sdk/src/index.d.ts
@@ -2,6 +2,5 @@ export { default as CompositeAudioNode } from './ParamMgr/CompositeAudioNode.js'
 export { default as ParamMgrNode } from './ParamMgr/ParamMgrNode.js';
 export { default as ParamMgrRegister } from './ParamMgr/ParamMgrRegister.js';
 export { default as WebAudioModule } from './WebAudioModule.js';
-export { WamParameterSab, WamParameterNoSab } from './WamParameter.js';
 export { default as WamParameterInfo } from './WamParameterInfo.js';
 export { default as WamNode } from './WamNode.js';
