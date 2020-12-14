6c67a8a3ee230a42c30ef79ef1bec42cdffed71c
Fr0stbyteR <renshihong@hotmail.com>
2020-12-14 17:50:55 +0800

    update CompositeAudioNode doc

--- packages/sdk/src/ParamMgr/CompositeAudioNode.d.ts
+++ packages/sdk/src/ParamMgr/CompositeAudioNode.d.ts
@@ -2,6 +2,12 @@
 
 import { WamNode } from '../api/types';
 
+/**
+ * The `CompositeAudioNode` can be used to mixin different `AudioNode`s into single one.
+ * In the `ParamMgr`'s case. the `ParamMgrNode` can be used to proxy the `WebAudioModule` interface,
+ * the `_wamNode`,
+ * then let other `AudioNode`s to connect to the host's audio graph, the `_output`.
+ */
 interface CompositeAudioNode extends Omit<GainNode, keyof EventTarget>, WamNode {
     /**
      * `undefined` by default, override it for proxying the interface
